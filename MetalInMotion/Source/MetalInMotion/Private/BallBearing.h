b5812fccbe0013f3cfa35ecfdcf3b7dc61b65942
Denis <kras_den@rambler.ru>
2022-12-04 13:52:15 +0200

    [26] properties

--- MetalInMotion/Source/MetalInMotion/Private/BallBearing.h
+++ MetalInMotion/Source/MetalInMotion/Private/BallBearing.h
@@ -18,8 +18,11 @@ public:
 	virtual void Tick(float DeltaTime) override;
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = BallBearing)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BallBearing")
 	UStaticMeshComponent* BallMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "BallBearing")
+	float Magnitize = 1.0f;
 
 protected:
 	virtual void BeginPlay() override;
